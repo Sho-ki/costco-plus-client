aff5cc661aae3cf0ba3bae47ac3ea90fedb992a5
Sho-ki <shoki0116.highjump@gmail.com>
2025-01-19 23:17:15 +0900

    update nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	images: {
 		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'wp.com',
+			},
 			{
 				protocol: 'https',
 				hostname: 'ikkoss.com',
