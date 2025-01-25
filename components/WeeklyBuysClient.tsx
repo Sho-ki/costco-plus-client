8e774155bd9b2fe0e889ce08c03660701611f9fb
Sho-ki <shoki0116.highjump@gmail.com>
2025-01-26 01:19:39 +0900

    remove custom

--- components/WeeklyBuysClient.tsx
+++ components/WeeklyBuysClient.tsx
@@ -5,13 +5,36 @@ import { fetchWeeklyBuys } from "../utils/api";
 import ProductCard from "./ProductCard";
 import { ProductForUsers } from "../types/product";
 import GoogleAdsense from "./GoogleAdsense";
-import { IN_FEED_SLOTS } from "../lib/inFeedSlots";
 
 interface WeeklyBuysClientProps {
   warehouseId: number;
   field: "price" | "discountPercentage";
   order: "asc" | "desc";
 }
+
+const IN_FEED_SLOTS = [
+	{
+		dataLayoutKey: '-6r+ed+2h-1n-4u',
+		dataAdSlot: '5979911995',
+	},
+	{
+		dataLayoutKey: '+3h+pw-t-80+pj',
+		dataAdSlot: '1607189193',
+	},
+	{
+		dataLayoutKey: '+3m+pw-l-78+mx',
+		dataAdSlot: '8854020173',
+	},
+	{
+		dataLayoutKey: '+3q+pw-l-78+mx',
+		dataAdSlot: '8103775785',
+	},
+	{
+		dataLayoutKey: '-ex+5u+4t-da+6l',
+		dataAdSlot: '2921105018',
+	},
+];
+
 
 export default function WeeklyBuysClient({
   warehouseId,
