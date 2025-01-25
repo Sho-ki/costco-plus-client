d8105278a2da1030499a2ea1174637b5e29b6579
Sho-ki <shoki0116.highjump@gmail.com>
2025-01-25 11:08:52 +0900

    add adsense

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -9,12 +9,24 @@ const GoogleAdsense: React.FC<Props> = ({ pId }) => {
     return null;
   }
   return (
+    <>
     <Script
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
       crossOrigin="anonymous"
       strategy="afterInteractive"
     />
+    <ins className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-format="fluid"
+          data-ad-layout-key="+3m+pw-l-78+mx"
+          data-ad-client={`ca-pub-${pId}`}
+          data-ad-slot="8854020173">
+    </ins>
+    <Script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+    </Script>
+      </>
   );
 };
 
