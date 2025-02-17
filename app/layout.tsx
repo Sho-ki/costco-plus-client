fecd2e8d74b32d831a1be3066140e3d9b4e4bb9c
Sho-ki <shoki0116.highjump@gmail.com>
2025-02-17 20:37:58 +0900

    add adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        {/* <GoogleAdsense /> */}
+        <GoogleAdsense />
         <Footer />
       </body>
     </html>
